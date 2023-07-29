3f59f1192f20c5a1b20b4d03f01e691e3a0ac8fe
Michi Rieser <michi.rieser@careanesth.com>
2023-07-29 17:15:59 +0200

    Added MediatR for decoupling from entity framework. Added AutoMapper. Refactored adding services to the application.

--- API/Controllers/LogsController.cs
+++ API/Controllers/LogsController.cs
@@ -1,4 +1,6 @@
-﻿using Domain;
+﻿using Application.Logs;
+using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -7,23 +9,42 @@ namespace API.Controllers
 {
     public class LogsController : BaseApiController
     {
-        private readonly DataContext _context;
-
-        public LogsController(DataContext context)
-        {
-            _context = context;
-        }
-
         [HttpGet] // api/logs
         public async Task<ActionResult<List<Log>>> GetLogs()
         {
-            return await _context.Logs.ToListAsync();
+            // Get logs directly from entity framework
+            //return await _context.Logs.ToListAsync();
+
+            // Get logs from mediator which uses entity framework
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")] // api/logs/{id}
         public async Task<ActionResult<Log>> GetLog(Guid id)
         {
-            return await _context.Logs.FindAsync(id);
+            return await Mediator.Send(new Details.Query { Id = id });
+        }
+
+        [HttpPost] // api/logs
+        public async Task<IActionResult> CreateLog(Log log)
+        {
+            await Mediator.Send(new Create.Command { Log = log });
+            return Ok();
+        }
+
+        [HttpPut("{id}")] // api/logs/{id}
+        public async Task<IActionResult> EditLog(Guid id, Log log)
+        {
+            log.Id = id;
+            await Mediator.Send(new Edit.Command { Log = log });
+            return Ok();
+        }
+
+        [HttpDelete("{id}")] // api/logs/{id}
+        public async Task<IActionResult> DeleteLog(Guid id)
+        {
+            await Mediator.Send(new Delete.Command { Id = id });
+            return Ok();
         }
     }
 }
---
+++ API/Extensions/ApplicationServiceExtension.cs
@@ -0,0 +1,29 @@
+﻿using Application.Core;
+using Application.Logs;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            // Add MediatR to services
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(List.Handler).Assembly));
+
+            // Add AutoMapper to services
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return services;
+        }
+    }
+}
