ed53624a9ae59096b7a2dedc16a1fbc1b40bb99f
Michi Rieser <michi.rieser@careanesth.com>
2023-08-01 08:58:06 +0200

    Add profiles controller to get profile logs

---
+++ API/Controllers/ProfilesController.cs
@@ -0,0 +1,16 @@
+﻿using Application.Profiles;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class ProfilesController : BaseApiController
+    {
+        [HttpGet("{userName}/logs")]
+        public async Task<IActionResult> GetUserLogs(string userName)
+        {
+            return HandleResult(await Mediator.Send(new ListLogs.Query { UserName = userName }));
+        }
+    }
+}
---
+++ Application/Profiles/ListLogs.cs
@@ -0,0 +1,41 @@
+﻿using Application.Core;
+using Application.Logs;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Profiles
+{
+    public class ListLogs
+    {
+        public class Query : IRequest<Result<List<LogDto>>>
+        {
+            public string UserName { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Result<List<LogDto>>>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<Result<List<LogDto>>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var logs = await _context.Logs
+                    .Where(x => x.AppUser.UserName == request.UserName)
+                    .OrderBy(x => x.Date)
+                    .ProjectTo<LogDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync();
+
+                return Result<List<LogDto>>.Success(logs);
+            }
+        }
+    }
+}
